8fb6b9e91ce9284338a9ec01bf4e12a49165fc6b
Anders <andersravn94@gmail.com>
2023-11-08 10:20:08 +0100

    Cat JUnit Test

--- test/CatTest.java
+++ test/CatTest.java
@@ -1,4 +1,29 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CatTest {
-  
+    private Cat c;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        c = new Cat("Møffe", 'M', 8);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+        assertNotNull(c.getName());
+        assertFalse(c.getName().isEmpty());
+    }
+
+    @Test
+    void getGender() {
+        assertTrue(c.getGender() == 'M' || c.getGender() == 'f');
+    }
+
+    @Test
+    void getWeightInKg() {
+        assertTrue(c.getWeightInKg() > 0 && c.getWeightInKg() <= 10);
+    }
 }
